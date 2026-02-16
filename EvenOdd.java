fd39b03ad908cf036106697ab769c4af4dded13a
JANANI S <jananis_bbm27@mepcoeng.ac.in>
2026-02-16 15:36:23 +0545

    Add EvenOdd class with isEven method
    Implement isEven method to check if a number is even.

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+/*Input: n = 15
+Output: false
+Explanation: The number is not divisible by 2, Odd number.
+  */
+
+
+class EvenOdd {
+    static boolean isEven(int n) {
+        // code here
+        if(n%2==0) return true;
+        else{
+            return false;
+        }
+    }
+}
